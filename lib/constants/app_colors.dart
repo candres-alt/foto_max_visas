1a22ab09d52cf654c59d72cd75fd44f7414d3112
Carlos Andres Barberan <c.andres@fotomaxvisas.com>
2026-08-30 09:48:35 -0400

    Preparación de estructura para Actividad Integradora 2

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF1565C0);
+  static const Color secondary = Color(0xFFE31E24);
+  static const Color background = Color(0xFFF4F7FB);
+  static const Color textDark = Color(0xFF1F2937);
+}
